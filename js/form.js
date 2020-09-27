cd9675750c36089cde0bfaf5049605ff84319afd
DimkaVeselov <babunovd@mail.ru>
2020-09-27 18:25:25 +0300

    добавил контент

--- js/form.js
+++ js/form.js
@@ -2,18 +2,17 @@
 
 $(document).ready(function () {
 
-  //E-mail Ajax Send
-  $("form").submit(function () { //Change
+  $("form").submit(function () { 
     var th = $(this);
     $.ajax({
       type: "POST",
-      url: "mail.php", //Change
+      url: "mail.php", 
       data: th.serialize()
     }).done(function () {
       alert("Thank you!");
       setTimeout(function () {
         th.trigger("reset");
-      }, 500);
+      }, 1000);
     });
     return false;
   });
